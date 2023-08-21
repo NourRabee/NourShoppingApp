dd5760843195a6cf80f55692dc76c844b9443785
nour <rabee.nour123@gmail.com>
2023-08-21 17:00:10 +0300

    Repository Creation

---
+++ src/main/java/ps/exalt/shopping/app/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////
+//          author: Nour
+//          filename: CategoryRepository.java
+//          2023
+////////////////////////////////////////////////
+package ps.exalt.shopping.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ps.exalt.shopping.app.model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, String> {
+}
